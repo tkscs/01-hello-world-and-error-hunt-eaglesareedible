e35115d084368481cdf6b687bb0ddab153bfd622
eaglesareedible <zadler2029@kehillahstudent.org>
2025-08-28 09:03:56 -0700

    found some errors

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+print(25 % 7)
+
+
+
+#SyntaxError: unterminated string literal (detected at line 1)
+#NameError: name 'pint' is not defined. Did you mean: 'print'?
+#  print string ("This exists")  
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+#IndentationError: unexpected indent
+#ValueError: invalid literal for int() with base 10: 'This exists'
+#  print{"hello there"}
+#    ^^^^^^^^^^^^^^^^^^^^
+#SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+#  print["hi"]
+#    ~~~~~^^^^^^
+#ypeError: 'builtin_function_or_method' object is not subscriptable
